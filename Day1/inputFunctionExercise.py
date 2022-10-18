05eaf6339f2793dd56ab01b336a339272f3fec2e
Aline Soliman <alineapreis@hotmail.com>
2022-10-18 19:02:16 -0300

    feat: add variables into input exercise

--- Day1/inputFunctionExercise.py
+++ Day1/inputFunctionExercise.py
@@ -2,3 +2,7 @@ Instructions
 # Write a program that prints the number of characters in a user's name. 
 
 print(len(input('What is your name?')))
+
+name = input('What is your name?')
+length = len(name)
+print(name)
