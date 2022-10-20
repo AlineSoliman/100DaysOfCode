4b34fca8b82f2dd80dd357408ea2306426317bdb
Aline Soliman <alineapreis@hotmail.com>
2022-10-20 19:17:05 -0300

    feat: add comments at IMB calculator exercises file

--- Day2/BMI_calculator.py
+++ Day2/BMI_calculator.py
@@ -5,6 +5,13 @@
 
 # The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m):
 
+# Example Input
+# weight = 80
+# height = 1.75
+# Example Output
+# 80 ÷ (1.75 x 1.75) = 26.122448979591837
+# 26
+
 # 🚨 Don't change the code below 👇
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
